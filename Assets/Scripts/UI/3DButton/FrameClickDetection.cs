818f5c717bf6d308d1c0ef1b116a688582dd266a
Janik Mueller <mueller@cqse.eu>
2019-06-18 14:43:38 +0200

    Added Advanced Menu scene, 3D Button and Slider

---
+++ Assets/Scripts/UI/3DButton/FrameClickDetection.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameClickDetection : MonoBehaviour
+{
+    public int clickcount;
+    public bool wait;
+    private Collider pressurePlateCollider;
+    private Transform pressurePlateTransform;
+    private MeshRenderer meshRenderer;
+    public Color defaultColor;
+
+    private void Start()
+    {
+        clickcount = 0;
+        wait = false;
+        pressurePlateTransform = transform.parent.GetChild(0);
+        pressurePlateCollider = pressurePlateTransform.GetComponent<Collider>();
+        defaultColor = transform.GetChild(0).GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (pressurePlateCollider.Equals(other))
+        {
+            if (!wait)
+            { 
+                Click();
+                highlightOn();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (pressurePlateCollider.Equals(other))
+        {
+            if (pressurePlateTransform.position.z < transform.position.z)
+            {
+                wait = false;
+                highlightOff();
+            }
+            else
+            {
+                wait = true;
+            }
+        }
+    }
+
+    void Click()
+    {
+        clickcount++;
+        Debug.Log("Clicks: " + clickcount);
+    }
+
+    void highlightOn()
+    {
+        Debug.Log("Start Highlight");
+        foreach(MeshRenderer childMeshRenderer in transform.GetComponentsInChildren<MeshRenderer>())
+        {
+            childMeshRenderer.material.SetColor("_BaseColor", Color.red);
+        }
+    }
+
+    void highlightOff()
+    {
+        foreach(MeshRenderer childMeshRenderer in transform.GetComponentsInChildren<MeshRenderer>())
+        {
+            childMeshRenderer.material.SetColor("_BaseColor", defaultColor);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/3DButton/PressurePlate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Vector3 localStartPos;
+    public int activeCollisions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localStartPos = transform.localPosition;
+        activeCollisions = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeCollisions == 0)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, localStartPos, Time.deltaTime * 1.5f);
+            if (Mathf.Abs(transform.localPosition.z - localStartPos.z) < 0.05)
+            {
+                transform.localPosition = localStartPos;
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        activeCollisions++;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        activeCollisions--;
+    }
+}
