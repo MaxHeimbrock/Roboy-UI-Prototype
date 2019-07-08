049305245026c7058b8b6121509ae4824354fb71
Leenert Specht <leenert.specht@tum.de>
2019-07-08 13:22:26 +0200

    Updated animation, reimported all assets

---
+++ Assets/Scripts/Transition/ResetCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ResetCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+        Valve.VR.OpenVR.Compositor.SetTrackingSpace(
+        Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputTracking.disablePositionalTracking = true;
+    }
+}
