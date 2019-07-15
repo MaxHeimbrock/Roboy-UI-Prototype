ab3f6c1f3fe5a8a11d65b3aa0bcd6dbf1aced893
Leenert Specht <leenert.specht@gmail.com>
2019-07-15 15:19:42 +0200

    initial add of ros sharp

---
+++ Assets/Scripts/ROS/TestPublisher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RosSharp.RosBridgeClient;
+
+public class TestPublisher : Publisher<RosSharp.RosBridgeClient.Messages.Standard.String>
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    private void publishMessage(string message)
+    {
+        RosSharp.RosBridgeClient.Messages.Standard.String rosString = new RosSharp.RosBridgeClient.Messages.Standard.String(message);
+        Publish(rosString);
+    }
+
+}
