0779164beae78028fea2efa369ecfd680ba4b790
Max Heimbrock <max.heimbrock@gmx.de>
2019-07-16 16:29:04 +0200

    Log text added, has to be implemented still

---
+++ Assets/LogText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogText : Singleton<LogText>
+{
+    private string text = "No Log";
+
+    private int lines = 0;
+
+    private TextMesh textMesh;
+
+    public void Start()
+    {
+        textMesh = this.gameObject.GetComponent<TextMesh>();
+        textMesh.text = text;
+    }
+
+    public void addToLogText(string message)
+    {
+        text += message;
+        text += "\n";
+
+        lines++;
+    }
+}
