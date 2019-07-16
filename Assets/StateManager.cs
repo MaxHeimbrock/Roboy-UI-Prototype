844bf6372c6bcc5c39dd390c4538670079266e46
Max Heimbrock <max.heimbrock@gmx.de>
2019-07-16 15:35:05 +0200

    Added StateManager as singleton

---
+++ Assets/StateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager: Singleton<StateManager>
+{
+    public enum MenuState {HUD, transitionToAdvancedMenu, advancedMenu, transitionToHUD};
+
+    private MenuState currentMenuState = MenuState.HUD;
+
+    public void GoToState(MenuState menuState)
+    {
+        currentMenuState = menuState;
+    }
+
+    public void GoToNextState()
+    {
+        if (currentMenuState != MenuState.transitionToHUD)
+            currentMenuState++;
+
+        else
+            currentMenuState = MenuState.HUD;
+
+        Debug.Log("Changed state to " + currentMenuState);
+    }
+}
