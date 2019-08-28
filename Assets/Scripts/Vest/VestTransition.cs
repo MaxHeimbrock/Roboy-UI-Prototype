cdc1b9eb617ae4f7bdf13860d46192aedb63a7f7
Leenert Specht <leenert.specht@tum.de>
2019-08-28 20:35:09 +0200

    integrated vest into transition

---
+++ Assets/Scripts/Vest/VestTransition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bhaptics.Tact.Unity;
+using UnityEngine;
+
+public class VestTransition : MonoBehaviour
+{
+    public TactSource[] Sources;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            playTact();
+        }
+    }
+
+    public void playTact() {
+        StartCoroutine(playTactSources());
+    }
+
+    IEnumerator playTactSources() {
+        foreach (var tactSource in Sources)
+        {
+            tactSource.Play();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
