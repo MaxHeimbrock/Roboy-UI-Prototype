1c3574ed9b5a9232c5df76850da7c3d8634074e0
Janik Müller <janik.mueller@gmx.de>
2019-08-30 21:41:24 +0200

    connect slider to rotate roboy

---
+++ Assets/Scripts/FinalsDemo/RotateRoboy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRoboy : MonoBehaviour
+{
+    CustomSlider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] sliders = GameObject.FindGameObjectsWithTag("Slider3D");
+        for (int i = 0; i < sliders.Length; i++)
+        {
+            if(sliders[i].name.Equals("RotateRoboySlider"))
+            {
+                slider = sliders[i].GetComponent<CustomSlider>();
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localEulerAngles = new Vector3(this.transform.localEulerAngles.x, slider.GetValue() * 360, this.transform.localEulerAngles.z);
+    }
+}
