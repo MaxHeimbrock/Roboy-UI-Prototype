53bad3429e6cc35191a7f3b9b66af78099147d90
Leenert Specht <leenert.specht@tum.de>
2019-08-30 15:37:04 +0200

    added test service publisher

---
+++ Assets/Scripts/ROS/TestServicePublisher.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+using RosSharp.RosBridgeClient;
+using RosSharp.RosBridgeClient.Services.Roboy;
+
+public class TestServicePublisher: MonoBehaviour
+{
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {   
+            callService();
+        }
+    }
+
+    /// <summary>
+    /// This method publishes a simple string messages to the topic of the object.
+    /// </summary>
+    /// <param name="message">is the message, which shall be published.</param>
+    public void callService() {
+        RosConnector rosConnector = GetComponent<RosConnector>();
+        
+        VRPuppetStateTransmissionRequest vrPuppetStateTransmissionRequest = new VRPuppetStateTransmissionRequest(true);
+        rosConnector.RosSocket.CallService<VRPuppetStateTransmissionRequest, VRPuppetStateTransmissionResponse>("/vr_puppets/state_transmission", ServiceCallHandler,
+            new VRPuppetStateTransmissionRequest(true));
+    }
+    private void ServiceCallHandler(VRPuppetStateTransmissionResponse message)
+    {
+        Debug.Log("ROS Distro: " + message.success);
+    }
+
+}
