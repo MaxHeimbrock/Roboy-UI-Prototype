f73da952d6fbfea11cfb212de67d4884930de595
Leenert Specht <leenert.specht@tum.de>
2019-08-30 18:37:42 +0200

    further changes

--- Assets/Scripts/ROS/TestServicePublisher.cs
+++ Assets/Scripts/ROS/TestServicePublisher.cs
@@ -12,15 +12,11 @@ public class TestServicePublisher: MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// This method publishes a simple string messages to the topic of the object.
-    /// </summary>
-    /// <param name="message">is the message, which shall be published.</param>
     public void callService() {
         RosConnector rosConnector = GetComponent<RosConnector>();
         
         VRPuppetStateTransmissionRequest vrPuppetStateTransmissionRequest = new VRPuppetStateTransmissionRequest(true);
-        rosConnector.RosSocket.CallService<VRPuppetStateTransmissionRequest, VRPuppetStateTransmissionResponse>("/vr_puppets/state_transmission", ServiceCallHandler,
+        rosConnector.RosSocket.CallService<VRPuppetStateTransmissionRequest, VRPuppetStateTransmissionResponse>("/roboy_middleware_msgs/VRPuppetStateTransmission", ServiceCallHandler,
             new VRPuppetStateTransmissionRequest(true));
     }
     private void ServiceCallHandler(VRPuppetStateTransmissionResponse message)
