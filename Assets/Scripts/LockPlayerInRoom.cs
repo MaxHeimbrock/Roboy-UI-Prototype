b5a41ea237ecb8d3a5009b1d5a998be729e6374b
Janik Müller <janik.mueller@tum.de>
2019-08-31 11:28:58 +0200

    Player is locked in the room

---
+++ Assets/Scripts/LockPlayerInRoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This only works, if the simple camera controller is disabled
+/// </summary>
+public class LockPlayerInRoom : MonoBehaviour
+{
+    Vector3 lastPos;
+
+    bool lockZ = false;
+    bool lockX = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Wand oben || Wand unten
+        if (this.transform.position.z >= 4.6 || this.transform.position.z <= -2.5f)
+            lockZ = true;
+
+        // Wand links || Wand rechts unten || Wand rechts oben
+        if (this.transform.position.x <= -4.85f || (this.transform.position.x >= 1.39f && this.transform.position.z > 1.79f) || (this.transform.position.x <= -0.4f &&  this.transform.position.z < 1.79f))
+            lockX = true;
+
+        if (lockX == true && lockZ == false)
+            this.transform.position = new Vector3(lastPos.x, this.transform.position.y, this.transform.position.z);
+        else if (lockX == false && lockZ == true)
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, lastPos.z);
+        else if (lockX == true && lockZ == true)
+            this.transform.position = new Vector3(lastPos.x, this.transform.position.y, lastPos.z);
+
+        lastPos = this.transform.position;
+        lockX = false;
+        lockZ = false;
+    }
+}
