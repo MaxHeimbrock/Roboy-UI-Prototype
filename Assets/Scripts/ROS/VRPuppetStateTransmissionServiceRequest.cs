f29adb25a584d192c601792ddcc8497014145ed4
Leenert Specht <leenert.specht@tum.de>
2019-08-31 11:40:10 +0200

    tested with vrpuppet, removed code for demo, created prefab

--- Assets/Scripts/ROS/VRPuppetStateTransmissionServiceRequest.cs
+++ Assets/Scripts/ROS/VRPuppetStateTransmissionServiceRequest.cs
@@ -3,37 +3,22 @@ using System;
 using RosSharp.RosBridgeClient;
 using RosSharp.RosBridgeClient.Services.Roboy;
 
-public class VRPuppetStateTransmissionServiceRequest : Singleton<VRPuppetStateTransmissionServiceRequest>
-{
-    public void Start()
-    {
-        /* For testing purposes, comment OUT for FINAL DEMO */
-        //Advertise the service
-        RosConnector rosConnector = GetComponent<RosConnector>();
-        rosConnector.RosSocket.AdvertiseService<VRPuppetStateTransmissionRequest, VRPuppetStateTransmissionResponse>("/roboy_middleware_msgs/VRPuppetStateTransmission", ServiceResponseHandler);
-    }
-
-    public void callService()
-    {
-        // Call the service VRPuppetStateTransmission when transitioning to Advanced Menu
-
+public class VRPuppetStateTransmissionServiceRequest : Singleton<VRPuppetStateTransmissionServiceRequest> {
+    /// <summary>
+    /// Calls vrpuppets service in order to move the motors during the transition
+    /// </summary>
+    public void callService() {
         RosConnector rosConnector = GetComponent<RosConnector>();
         VRPuppetStateTransmissionRequest vrPuppetStateTransmissionRequest = new VRPuppetStateTransmissionRequest(true);
-        rosConnector.RosSocket.CallService<VRPuppetStateTransmissionRequest, VRPuppetStateTransmissionResponse>("/roboy_middleware_msgs/VRPuppetStateTransmission", ServiceCallHandler,
-            new VRPuppetStateTransmissionRequest(true));
+        // we need to use '/vr_puppets/state_transmission' for the demo
+        rosConnector.RosSocket.CallService<VRPuppetStateTransmissionRequest, VRPuppetStateTransmissionResponse>("/vr_puppets/state_transmission", ServiceCallHandler, new VRPuppetStateTransmissionRequest(true));
     }
 
-    // Mock handler for receiving requests
-    private void ServiceCallHandler(VRPuppetStateTransmissionResponse message)
-    {
-        Debug.Log("Message is " + message.success);
-        Debug.Log("Message: " + message.message);
-    }
-
-    // Example for sending a response to a call 
-      private static bool ServiceResponseHandler(VRPuppetStateTransmissionRequest arguments, out VRPuppetStateTransmissionResponse result)
-    {
-        result = new VRPuppetStateTransmissionResponse(true, "transition init");       
-        return true;
+    /// <summary>
+    /// Handles the response of the service call
+    /// </summary>
+    /// <param name="message">incoming message</param>
+    private void ServiceCallHandler(VRPuppetStateTransmissionResponse message) {
+        Debug.Log("VRPuppet Service answered with " + message.success);
     }
 }
