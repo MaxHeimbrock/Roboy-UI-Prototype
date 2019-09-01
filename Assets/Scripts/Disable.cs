fc451634589561873434e694819cb09c29803cd8
Max Heimbrock <max.heimbrock@gmx.de>
2019-09-01 14:14:27 +0200

    test 3D button new instantiation

--- Assets/Scripts/Disable.cs
+++ Assets/Scripts/Disable.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Disable : MonoBehaviour
 {
+    public GameObject buttonPrefab;
+    public GameObject instance;
+
     private void OnDisable()
     {
         Debug.Log("Disabled");
+        componentDisEnable(this.gameObject, false);
+        recursiveDisEnable(this.gameObject, false);
     }
 
     private void OnEnable()
     {
         Debug.Log("Enabled");
+        componentDisEnable(this.gameObject, true);
+        recursiveDisEnable(this.gameObject, true);
+
+        instance = Instantiate(buttonPrefab, this.transform.position + new Vector3(3f,0,0), this.transform.rotation);
+    }
+
+    private void recursiveDisEnable(GameObject parent, bool enable)
+    {
+        Debug.Log("Childcount: " + parent.transform.childCount);
+        for(int i = 0; i < parent.transform.childCount; i++)
+        {
+            recursiveDisEnable(parent.transform.GetChild(i).gameObject, enable);
+        }
+        parent.SetActive(enable);
+    }
+
+    private void componentDisEnable(GameObject parent, bool enable)
+    {
+        foreach(Collider coll in parent.GetComponentsInChildren<Collider>())
+        {
+            coll.enabled = enable;
+        }
+
+        foreach(FrameClickDetection fcd in parent.GetComponentsInChildren<FrameClickDetection>())
+        {
+            fcd.enabled = enable;
+        }
     }
 }
