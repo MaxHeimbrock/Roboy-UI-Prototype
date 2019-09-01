a50a4771a3330642143bcb7f87bcf6668500b17a
Janik Müller <janik.mueller@gmx.de>
2019-09-01 14:12:12 +0200

    TableColliderArea added as trigger collider

---
+++ Assets/tableTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tableTrigger : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "palmCollider")
+        {
+            Debug.Log("Table triggered");
+            VRPuppetStateTransmissionServiceRequest.Instance.CallHandService(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {        
+        if (other.name == "palmCollider")
+        {
+            Debug.Log("Table collider left");
+            VRPuppetStateTransmissionServiceRequest.Instance.CallHandService(false);
+
+        }
+    }
+}
