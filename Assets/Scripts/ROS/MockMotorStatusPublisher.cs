1e0a0a5c8a5c318ee5520964295a4a43119fbe59
Marla Narazani <marla.narazani@tum.de>
2019-09-01 13:05:17 +0200

    added motor pub & sub and sample motors

---
+++ Assets/Scripts/ROS/MockMotorStatusPublisher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RosSharp.RosBridgeClient;
+
+public class MockMotorStatusPublisher : Publisher<RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus>
+{
+
+    protected override void Start()
+    {
+        StartCoroutine(StartPublisher(1.0f));
+    }
+
+    private IEnumerator StartPublisher(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            base.Start();
+
+            // TEST MESSAGE FOR MOTOR STATUS      
+            RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus message = new RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus();
+            message.id = 0;
+            message.power_sense = true;
+            message.pwm_ref = new int[] {10};
+            message.position = new int[] {2};
+            message.velocity = new int[] {20};
+            message.displacement = new int[] {0};
+            message.current = new short[] {2};
+            message.angle = new int[] {20};
+            PublishMessage(message);
+
+             // TEST MESSAGE FOR MOTOR STATUS      
+            RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus message_0 = new RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus();
+            message.id = 1;
+            message.power_sense = true;
+            message.pwm_ref = new int[] {10};
+            message.position = new int[] {2};
+            message.velocity = new int[] {20};
+            message.displacement = new int[] {0};
+            message.current = new short[] {2};
+            message.angle = new int[] {20};
+
+            PublishMessage(message);
+            yield return new WaitForSeconds(waitTime);
+            PublishMessage(message_0);
+            break;
+        }
+    }
+
+    private void PublishMessage(RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus message)
+    {
+        Publish(message);
+    }
+}
---
+++ Assets/Scripts/ROS/MotorStatusSubscriber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using RosSharp.RosBridgeClient;
+using RosSharp.RosBridgeClient.Messages.Roboy;
+
+public class MotorStatusSubscriber : Subscriber<RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus>
+{
+    protected override void Start()
+    {
+        StartCoroutine(startSubscriber(1.0f));
+    }
+
+    private IEnumerator startSubscriber(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            base.Start();
+            break;
+        }
+    }
+
+    protected override void ReceiveMessage(RosSharp.RosBridgeClient.Messages.Roboy.MotorStatus message)
+    {
+        Debug.Log("ID: " + message.id);
+        Debug.Log("Power: " + message.power_sense);
+        Debug.Log("PWM: " + message.pwm_ref);
+    }
+}
