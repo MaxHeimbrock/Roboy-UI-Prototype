ff560dab64fc8459193ad4cbebfb073088fde52e
Janik Müller <janik.mueller@gmx.de>
2019-09-01 12:40:25 +0200

    add disable script

---
+++ Assets/Scripts/Disable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        Debug.Log("Disabled");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Enabled");
+    }
+}
